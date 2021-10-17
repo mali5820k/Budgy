2010650513f6d0484f10f87493f695763628236f
mali5820k <mali5820k@gmail.com>
2021-10-16 22:53:53 -0400

    Initial files

---
+++ Compiler.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KraitScript {
+
+	public class Compiler {
+		public static void Main(string[] args) {
+			Console.Writeline("New Project Compiler");
+		}
+
+		public bool generateIL() {
+			// Placeholder
+		}
+	}
+	
+}
---
+++ Lexer.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KraitScript {
+
+	public class Lexer {
+		public static void Main(string[] args) {
+			Console.Writeline("C#");
+		}
+	}
+	
+}
