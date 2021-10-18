c03a62abc43751b85c0afaefc558e02aec1c3e35
mali5820k <mali5820k@gmail.com>
2021-10-18 16:11:41 -0400

    Update Lexer.cs

--- Lexer.cs
+++ Lexer.cs
@@ -6,6 +6,67 @@ using System.Threading.Tasks;
 
 namespace KraitScript {
 
+	struct keyWords {
+		VAR,
+		FOR,
+		WHILE,
+		FUNC,
+		PRINT,
+		INT,
+		FLOAT,
+		STRING,
+		CHAR,
+		DOUBLE,
+		LONG,
+		BYTE,
+		BIT,
+		CLASS,
+		OBJECT,
+		RETURN,
+		CONTINUE,
+		BREAK,
+		MAIN
+	}
+
+	struct operators {
+		PLUS,
+		PLUS_EQUAL,
+		POST_PLUS_PLUS,
+		PRE_PLUS_PLUS,
+
+		MINUS,
+		MINUS_EQUAL,
+		POST_MINUS_MINUS,
+		PRE_MINUS_MINUS,
+
+		MULT,
+		MULT_EQUAL,
+
+		DIV,
+		DIV_EQUAL,
+
+		EQUAL_ASSIGN,
+		EQUAL_EQUAL,
+
+		GREATER_THAN,
+		LESS_THAN,
+		GREATER_THAN_EQUAL,
+		LESS_THAN_EQUAL,
+
+		LOGICAL_NOT,
+		LOGICAL_OR,
+		LOGICAL_AND,
+
+		BITWISE_NOT,
+		BITWISE_OR,
+		BITWISE_AND,
+		BITWISE_NAND,
+		BITWISE_XOR,
+		BITWISE_ONES_COMPLEMENT,
+		BITWISE_TWOS_COMPLEMENT
+	}
+
+
 	public class Lexer {
 		public static void Main(string[] args) {
 			Console.Writeline("C#");
