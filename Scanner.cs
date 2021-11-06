7705cd68a01f0c20c46db9cdfb3812354f6a46c1
mali5820k <mali5820k@gmail.com>
2021-11-06 14:00:17 -0400

    Create Scanner.cs

---
+++ Scanner.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KraitScript {
+
+	public class Scanner
+	{
+		int numLines;
+		public void readFile(string fileName) {
+
+		}
+		public string getNextLine() {
+			return
+		}
+	}
+	
+}
