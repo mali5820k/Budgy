e774ed08f73d4a488697dad2451d08918c2646cb
mali5820k <mali5820k@gmail.com>
2021-11-12 13:19:31 -0500

    Need to handle exception from Lexer

--- Compiler.cs
+++ Compiler.cs
@@ -9,12 +9,16 @@ namespace KraitScript {
 	public class Compiler {
 
 		public static void Main(string[] args) {
-			Console.Writeline("New Project Compiler");
-                        if(args.Length < 2) {
+			Console.WriteLine("New Project Compiler");
+                        Console.WriteLine("Current args and argLength {0}", args[0]);
+                        if(args.Length < 1) {
                                 Console.WriteLine("Usage: ./KSCompiler sourceFile.ks");
                                 //Application.Exit(); // This apparently doesn't work on linux
                                 Environment.Exit(-1);
                         }
+                        Lexer lex = new Lexer();
+                        lex.ReadFile(args[0]);
+                        Console.WriteLine(lex.generateTokens());
 		}
  
 		public bool generateIL() {
--- Lexer.cs
+++ Lexer.cs
@@ -7,31 +7,9 @@ using System.Threading.Tasks;
 
 namespace KraitScript
 {
-
-        // Scan the text in from the source file
-        public class CodeScanner
-        {
-                int numLines;
-                List<string> file;
-                int line;
-                public CodeScanner()
-                {
-                        file = new list<string>();
-                }
-                public void ReadFile(string fileName)
-                {
-                        // Dropping this in favor of File methods
-                        //file = System.IO.ReadAllLines(fileName);
-                        if (!File.Exists(fileName))
-                        {
-                                file.Concat((File.ReadAllLines(fileName)).ToList<string>());
-                        }
-                }
-        }
-
         public class Lexer
         {
-                enum TokenType
+                enum Token
                 {
                         // Keywords
                         VAR, FOR, WHILE, FUNC, PRINT, INT, FLOAT, STRING, CHAR,
@@ -53,26 +31,41 @@ namespace KraitScript
                         BITWISE_ONES_COMPLEMENT, BITWISE_TWOS_COMPLEMENT
                 }
 
-
                 List<Token> readTokens;
-                Token currentToken { get; }
+                List<string> fileLines;
+                Token currentToken { get; set; }
                 Token previousToken { get; }
-                CodeScanner fileScanner;
+                //CodeScanner fileScanner;
 
                 string FileName { get; set; }
 
                 int currTokenIndex;
+
+                int numLines;
+
+                int line;
+
                 public Lexer()
                 {
-                        readTokens = new List<keyWords>();
-                        fileScanner = new CodeScanner();
+                        readTokens = new List<Token>();
+                        fileLines = new List<string>();
+                }
+
+                public void ReadFile(string fileName)
+                {
+                        // Dropping this in favor of File methods
+                        //file = System.IO.ReadAllLines(fileName);
+                        if (!File.Exists(fileName))
+                        {
+                                fileLines.Concat((File.ReadAllLines(fileName)).ToList<string>());
+                        }
                 }
 
                 public bool generateTokens()
                 {
-                        fileScanner.ReadFile(FileName);
+                        ReadFile(FileName);
 
-                        foreach (string line in lines)
+                        foreach (string line in fileLines)
                         {
                                 // Divide the string without the white-spaces
                                 // Check each word scanned in to see what 
