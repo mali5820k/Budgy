8966c439883eb2ddfcf26fc183cfbf0759595c68
mali5820k <mali5820k@gmail.com>
2021-12-19 15:22:26 -0500

    Upgrade to .NET 6.0 from .NET 5.0

---
+++ Krait-Script_C#_Compiler/Compiler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace KraitScript {    
+    public class Compiler {
+
+		public static void Main(string[] args) {
+			Console.WriteLine("New Project Compiler");
+                        Console.WriteLine("Current args and argLength {0}", args[0]);
+                        if(args.Length < 1) {
+                                Console.WriteLine("Usage: ./KSCompiler sourceFile.ks");
+                                //Application.Exit(); // This apparently doesn't work on linux
+                                Environment.Exit(-1);
+                        }
+                        Lexer lex = new Lexer();
+                        lex.ReadFile(args[0]);
+                        Console.WriteLine(lex.generateTokens());
+		}
+ 
+		public bool generateIL() {
+			// Placeholder
+                        return true;
+		}
+	}
+}
