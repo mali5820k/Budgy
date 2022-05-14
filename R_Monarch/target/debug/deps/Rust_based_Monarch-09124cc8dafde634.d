630e1f76b61c86a214fe0875f00b25b28646a3a6
mali5820k <mali5820k@gmail.com>
2022-05-13 20:48:53 -0400

    folder name-change

---
+++ R_Monarch/target/debug/deps/Rust_based_Monarch-09124cc8dafde634.d
@@ -0,0 +1,7 @@
+/home/mali/Desktop/Github Projects/Monarch-Programming-Language/Rust-based-Monarch/target/debug/deps/Rust_based_Monarch-09124cc8dafde634: src/main.rs src/lexer.rs src/parser.rs
+
+/home/mali/Desktop/Github Projects/Monarch-Programming-Language/Rust-based-Monarch/target/debug/deps/Rust_based_Monarch-09124cc8dafde634.d: src/main.rs src/lexer.rs src/parser.rs
+
+src/main.rs:
+src/lexer.rs:
+src/parser.rs:
