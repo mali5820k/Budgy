18938519d9ab6f2d0cc56a9a53d7886bc4dcff2d
mali5820k <mali5820k@gmail.com>
2022-06-04 01:55:17 -0400

    Update MyBuild.py

--- MyBuild.py
+++ MyBuild.py
@@ -5,6 +5,7 @@ def main():
     os.system("javac *.java")
     os.system("jar cfe monarch.jar Main *.class")
     os.system("move monarch.jar ../")
+    os.system("rm *.class")
 
 
 if __name__ == "__main__": main()
