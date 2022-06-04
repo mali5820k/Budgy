f626d811d32b5ef07c74ea5b9ecf99bae02d7de7
mali5820k <mali5820k@gmail.com>
2022-06-04 02:01:24 -0400

    Automated custom script

--- MyBuild.py
+++ MyBuild.py
@@ -1,11 +1,12 @@
 import os
 
 def main():
+    os.system("powershell.exe rm monarch.jar")
     os.chdir("Java_Monarch")
     os.system("javac *.java")
     os.system("jar cfe monarch.jar Main *.class")
-    os.system("move monarch.jar ../")
-    os.system("rm *.class")
+    os.system("powershell.exe mv monarch.jar ../")
+    os.system("powershell.exe rm *.class")
 
 
 if __name__ == "__main__": main()
